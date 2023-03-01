5e4d61d044e5ba831af65b32978958fe4958dafa
rose <rosemarie_dalton@hotmail.com>
2023-03-01 15:51:37 -0500

    we did it! (in dora voice) reverts back to good stuff

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,16 +5,10 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.SparkMaxPIDController.ArbFFUnits;
-import com.revrobotics.SparkMaxRelativeEncoder;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
-import edu.wpi.first.wpilibj.PowerDistribution;
-import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -23,38 +17,38 @@ public class Intake extends SubsystemBase {
   // Subsystem parameters
   private static final int kPivotLeft = 9;
   private static final int kPivotRight = 10;
+  private static final int kRollerId = 17;
 
-  private static final double kNativeToRad = 2.0 * Math.PI;
-  private static final double kNominalVolt = 10.0;
+  private static final int kLeftEncoder = 7;
+  private static final int KRightEncoder = 8;
+  private static final double kTurnPerRotation = 0.25;
+  private static final double kOffsetLeft = 0.28;
+  private static final double kOffsetRight = 0.17;
+  private static final double kUpperSoftLimit = 0.15;
+  private static final double kLowwerSoftLimit = 0.03;
 
-  private static final double kNeutralRad = Math.PI / 2.0;
-  private static final double kEncOff = 3.0 * Math.PI / 2.0;
-  private static final double kZeroOffsetLeft = 0.63 + kEncOff;
-  private static final double kZeroOffsetRight = 0.0 + kEncOff;
-  private static final double kHorizontalPercentLeft = 0.025;
-  private static final double kHorizontalPercentRight = 0.035;
+  private static final double kQuickBoiRight = 2.5;
+  private static final double kSlowBoiRight = 2;
+  private static final double kQuickBoiLeft = 2.5;
+  private static final double kSlowBoiLeft = 2;
 
-  private static final double kTargetTolRad = Math.toRadians(5.0);
-  private static final double kP = 0.8; // have to continue tweaking
-  private static final double kI = 0.0;
-  private static final double kD = 0.000;
+  private static final double kUP = 0.18;
+  private static final double kDOWN = 0.01;
+  private static final double kConeIntake = 0.06;
+  private double m_targetLeft = kUP;
+  private double m_targetRight = kUP;
 
-  private static final double kAngVelRad = Math.toRadians(180.0);
-  private static final double kAngAccRed = Math.toRadians(60.0);
-  private static final TrapezoidProfile.Constraints kProfileConstraints =
-      new TrapezoidProfile.Constraints(kAngVelRad, kAngAccRed);
-
-  private static final double kRetractRad = Math.toRadians(90.0 + 72.0);
-  private static final double kExtendRad = Math.toRadians(90.0);
-  private static final double kRollerPercent = 0.5;
+  private static final double kRollerPercentCube = 0.7;
+  private static final double kRollerPercentCone = 1;
+  private static final double kCurrentThreshold = 15;
 
   // Member objects
   private final CANSparkMax m_pivotLeft = new CANSparkMax(kPivotLeft, MotorType.kBrushless);
   private final CANSparkMax m_pivotRight = new CANSparkMax(kPivotRight, MotorType.kBrushless);
+  private final CANSparkMax m_roller = new CANSparkMax(kRollerId, MotorType.kBrushless);
+
   private final DutyCycleEncoder m_dutyEncoderLeft = new DutyCycleEncoder(kLeftEncoder);
   private final DutyCycleEncoder m_dutyEncoderRight = new DutyCycleEncoder(KRightEncoder);
-
-  private static PowerDistribution m_pdp = new PowerDistribution(20, ModuleType.kRev);
 
   /** Creates a new Intake. */
   public Intake() {
@@ -64,41 +58,22 @@ public class Intake extends SubsystemBase {
     m_roller.burnFlash();
 
     m_pivotLeft.restoreFactoryDefaults();
+    m_pivotLeft.setIdleMode(IdleMode.kBrake);
     m_pivotLeft.setInverted(true);
-    m_pivotLeft.enableVoltageCompensation(kNominalVolt);
-
-    m_encoderLeft.setInverted(true);
-    m_encoderLeft.setPositionConversionFactor(kNativeToRad);
-    m_encoderLeft.setVelocityConversionFactor(kNativeToRad);
-    m_encoderLeft.setZeroOffset(kZeroOffsetLeft);
-
-    m_pidLeft.setFeedbackDevice(SparkMaxRelativeEncoder.Type.kQuadrature);
-    m_pidLeft.setPositionPIDWrappingEnabled(true);
-    m_pidLeft.setP(kP);
-    m_pidLeft.setD(kD);
-    m_pidLeft.setI(kI);
-    m_pidLeft.setSmartMotionMaxVelocity(kAngVelRad, 0);
-    m_pidLeft.setSmartMotionMaxAccel(kAngAccRed, 0);
-
-    m_pivotLeft.burnFlash();
 
     m_pivotRight.restoreFactoryDefaults();
     m_pivotRight.setIdleMode(IdleMode.kBrake);
-    m_pivotRight.enableVoltageCompensation(kNominalVolt);
 
-    m_encoderRight.setPositionConversionFactor(kNativeToRad);
-    m_encoderRight.setVelocityConversionFactor(kNativeToRad);
-    m_encoderRight.setZeroOffset(kZeroOffsetRight);
+    m_dutyEncoderLeft.setDistancePerRotation(kTurnPerRotation);
+    m_dutyEncoderRight.setDistancePerRotation(kTurnPerRotation);
+    m_dutyEncoderLeft.reset();
+    m_dutyEncoderRight.reset();
 
-    m_pidRight.setFeedbackDevice(m_encoderRight);
-    m_pidRight.setPositionPIDWrappingEnabled(true);
-    m_pidRight.setP(kP);
-    m_pidRight.setD(kD);
-    m_pidRight.setI(kI);
-    m_pidRight.setSmartMotionMaxVelocity(kAngVelRad, 0);
-    m_pidRight.setSmartMotionMaxAccel(kAngAccRed, 0);
-
+    m_pivotLeft.burnFlash();
     m_pivotRight.burnFlash();
+
+    // Stop intake by default
+    this.setDefaultCommand(this.stop());
   }
 
   @Override
@@ -112,58 +87,32 @@ public class Intake extends SubsystemBase {
       m_targetRight = (getLeftEncoder() + getRightEncoder()) / 2;
     }
 
-    // Clear motion profile if target is reached
-    if (this.onTarget()) {
-      this.m_profile = null;
-    }
-
-    // Set reference in periodic to allow for arbitrary PID computation
-    if (m_profile != null) {
-      // Pivot is tracking a velocity profile
-      final var profTime = Timer.getFPGATimestamp() - m_profileStart;
-      final var profTarget = m_profile.calculate(profTime);
-
-      m_pidLeft.setReference(
-          profTarget.position,
-          ControlType.kPosition,
-          0,
-          this.computeFeedForward(profTarget.position, kHorizontalPercentLeft),
-          ArbFFUnits.kPercentOut);
-      m_pidRight.setReference(
-          profTarget.position,
-          ControlType.kPosition,
-          0,
-          this.computeFeedForward(profTarget.position, kHorizontalPercentRight),
-          ArbFFUnits.kPercentOut);
+    if (isLeftInFastRange() && isRightInFastRange()) {
+      m_pivotLeft.set(motorSpeedLeft() * kQuickBoiLeft);
+      m_pivotRight.set(motorSpeedRight() * kQuickBoiRight);
     } else {
-      // Pivot has reached its target thus holding its position
-      m_pidLeft.setReference(
-          m_targetRad,
-          ControlType.kPosition,
-          0,
-          this.computeFeedForward(m_targetRad, kHorizontalPercentLeft),
-          ArbFFUnits.kPercentOut);
-      m_pidRight.setReference(
-          m_targetRad,
-          ControlType.kPosition,
-          0,
-          this.computeFeedForward(m_targetRad, kHorizontalPercentRight),
-          ArbFFUnits.kPercentOut);
+      m_pivotLeft.set(motorSpeedLeft() * kSlowBoiLeft);
+      m_pivotRight.set(motorSpeedRight() * kSlowBoiRight);
     }
 
-    m_angleEntry.setDouble(Math.toDegrees(m_encoderLeft.getPosition()));
-    m_percentEntry.setDouble(m_pivotLeft.getAppliedOutput());
-    m_targetEntry.setDouble(Math.toDegrees(m_targetRad));
-    m_pidEntry.setDouble(
-        this.computeFeedForward(m_encoderLeft.getPosition(), kHorizontalPercentLeft));
+    System.out.println(m_roller.getOutputCurrent());
+  }
 
-    System.out.printf(
-        "Pos L %.2f    Pos R %.2f    Pos* %.0f    FF %.3f    Cmd %.3f\n",
-        Math.toDegrees(m_encoderLeft.getPosition()),
-        Math.toDegrees(m_encoderRight.getPosition()),
-        Math.toDegrees(m_targetRad),
-        this.computeFeedForward(m_encoderLeft.getPosition(), kHorizontalPercentLeft),
-        m_pivotLeft.getAppliedOutput());
+  /**
+   * Stop the roller
+   *
+   * @return instant command
+   */
+  public Command stop() {
+    return this.run(() -> m_roller.set(0.0));
+  }
+
+  public Command spin(String gamePiece) {
+    return this.run(
+        () -> {
+          if (gamePiece == "cube") m_roller.set(kRollerPercentCube);
+          else m_roller.set(kRollerPercentCone);
+        });
   }
 
   /**
@@ -171,8 +120,8 @@ public class Intake extends SubsystemBase {
    *
    * @return A value of 0 when the intake is down
    */
-  private double computeFeedForward(double angle, double horizontalPercent) {
-    return horizontalPercent * Math.cos(angle - Math.PI / 2.0);
+  private double getLeftEncoder() {
+    return 1 - m_dutyEncoderLeft.getAbsolutePosition() - kOffsetLeft;
   }
 
   /**
@@ -188,16 +137,8 @@ public class Intake extends SubsystemBase {
     return getLeftEncoder() > kLowwerSoftLimit && getLeftEncoder() < kUpperSoftLimit;
   }
 
-  /**
-   * Set the roller speed and pivot to a given angle
-   *
-   * @param rollerPercent roller speed (percent)
-   * @param pivotRad angle above horizontal (rad)
-   * @return blocking command
-   */
-  public Command operate(double rollerPercent, double pivotRad) {
-    return this.runOnce(() -> this.generateProfile(pivotRad))
-        .andThen(this.run(() -> m_roller.set(rollerPercent)).until(this::onTarget));
+  private boolean isRightInFastRange() {
+    return getRightEncoder() > kLowwerSoftLimit && getRightEncoder() < kUpperSoftLimit;
   }
 
   public Command setTarget(String position) {
@@ -220,11 +161,11 @@ public class Intake extends SubsystemBase {
     return m_targetLeft - getLeftEncoder();
   }
 
-  public Command spin() {
-    return this.run(() -> m_roller.set(kRollerPercent));
+  public double motorSpeedRight() {
+    return m_targetRight - getRightEncoder();
   }
 
   private void coneLift() {
-    if (m_pdp.getCurrent(7) < kCurrentThreshold) setTarget("cone");
+    if (m_roller.getOutputCurrent() > kCurrentThreshold) setTarget("cone");
   }
 }
