f9dfecd0d2e1122cfdddaaa72abf9921788694dc
rose <rosemarie_dalton@hotmail.com>
2023-03-05 21:38:14 -0500

    implement limitSwitch & update intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -17,15 +17,12 @@ public class Intake extends SubsystemBase {
 
   // Subsystem parameters
   private static final int kPivotLeft = 9;
-  private static final int kPivotRight = 10;
-  // private static final int kWheelsLeft = 11;
-  // private static final int kWheelsRight = 12;
+  private static final int kWheelsLeft = 17;
+  private static final int kWheelsRight = 18;
 
-  private static final int kLeftEncoder = 7;
-  private static final int KRightEncoder = 8;
+  private static final int kEncoder = 7;
   private static final double kTurnPerRotation = 0.25;
-  private static final double kOffsetLeft = 0.28;
-  private static final double kOffsetRight = 0.17;
+  private static final double kOffset = 0.28;
   /* private static final double kUpperSoftLimit = 0.15;
   private static final double kLowwerSoftLimit = 0.03;
 
@@ -39,46 +36,37 @@ public class Intake extends SubsystemBase {
   private static final double kConeIntake = 0.06;
   private static final double kWheelSpeed2nd = 0.5;
   private static final double kWheelSpeed3rd = 1.0;
-  private double m_targetLeft = kUP;
-  private double m_targetRight = kUP;
+  private double m_target = kUP;
   private double m_wheelSpeed = 0;
   private static double deadzone = 0.01;
   private static double angle = 70;
   private static double kNativeToAngle = 90 / 2048;
 
   // Member objects
-  private final CANSparkMax m_pivotLeft = new CANSparkMax(kPivotLeft, MotorType.kBrushless);
-  private final CANSparkMax m_pivotRight = new CANSparkMax(kPivotRight, MotorType.kBrushless);
-  // private final CANSparkMax m_wheelsLeft = new CANSparkMax(kWheelsLeft, MotorType.kBrushless);
-  // private final CANSparkMax m_wheelsRight = new CANSparkMax(kWheelsRight, MotorType.kBrushless);
-  private final DutyCycleEncoder m_dutyEncoderLeft = new DutyCycleEncoder(kLeftEncoder);
-  private final DutyCycleEncoder m_dutyEncoderRight = new DutyCycleEncoder(KRightEncoder);
-  private final DigitalInput m_limitSwitch = new DigitalInput(9);
+  private final CANSparkMax m_pivot = new CANSparkMax(kPivotLeft, MotorType.kBrushless);
+  private final CANSparkMax m_wheelsLeft = new CANSparkMax(kWheelsLeft, MotorType.kBrushless);
+  private final CANSparkMax m_wheelsRight = new CANSparkMax(kWheelsRight, MotorType.kBrushless);
+  private final DutyCycleEncoder m_dutyEncoder = new DutyCycleEncoder(kEncoder);
+  private final DigitalInput m_limitSwitch = new DigitalInput(8);
 
   // Subsystem parameters
 
   /** Creates a new Intake. */
   public Intake() {
 
-    m_pivotLeft.restoreFactoryDefaults();
-    m_pivotLeft.setIdleMode(IdleMode.kBrake);
-    m_pivotLeft.setInverted(true);
-
-    m_pivotRight.restoreFactoryDefaults();
-    m_pivotRight.setIdleMode(IdleMode.kBrake);
+    m_pivot.restoreFactoryDefaults();
+    m_pivot.setIdleMode(IdleMode.kBrake);
+    m_pivot.setInverted(true);
 
     // m_wheelsLeft.setIdleMode(IdleMode.kCoast);
 
     //  m_wheelsRight.setIdleMode(IdleMode.kCoast);
     //  m_wheelsRight.setInverted(true);
 
-    m_dutyEncoderLeft.setDistancePerRotation(kTurnPerRotation);
-    m_dutyEncoderRight.setDistancePerRotation(kTurnPerRotation);
-    m_dutyEncoderLeft.reset();
-    m_dutyEncoderRight.reset();
+    m_dutyEncoder.setDistancePerRotation(kTurnPerRotation);
+    m_dutyEncoder.reset();
 
-    m_pivotLeft.burnFlash();
-    m_pivotRight.burnFlash();
+    m_pivot.burnFlash();
     // m_wheelsLeft.burnFlash();
     // m_wheelsRight.burnFlash();
   }
@@ -86,13 +74,14 @@ public class Intake extends SubsystemBase {
   @Override
   public void periodic() {
 
+    limitSwitch();
+
     // Set target to current when robot is disabled to prevent sudden motion on enable
     if (DriverStation.isDisabled()) {
-      m_targetLeft = (getLeftEncoder() + getRightEncoder()) / 2;
-      m_targetRight = (getLeftEncoder() + getRightEncoder()) / 2;
+      m_target = (getLeftEncoder());
     }
 
-    System.out.println("hello");
+    System.out.println(limitSwitch());
     /*
     if (isLeftInFastRange() && isRightInFastRange()) {
       m_pivotLeft.set(motorSpeedLeft() * kQuickBoiLeft);
@@ -105,8 +94,8 @@ public class Intake extends SubsystemBase {
 
     // System.out.print("target " + m_targetLeft);
 
-    // m_wheelsLeft.set(m_wheelSpeed);
-    // m_wheelsRight.set(m_wheelSpeed);
+    m_wheelsLeft.set(m_wheelSpeed);
+    m_wheelsRight.set(m_wheelSpeed);
   }
   /**
    * Gets the value of the left encoder between 0 and 0.18
@@ -114,16 +103,7 @@ public class Intake extends SubsystemBase {
    * @return A value of 0 when the intake is down
    */
   private double getLeftEncoder() {
-    return 1 - m_dutyEncoderLeft.getAbsolutePosition() - kOffsetLeft;
-  }
-
-  /**
-   * Gets the value of the right encoder between 0 and 0.18
-   *
-   * @return A value of 0 when the intake is down
-   */
-  private double getRightEncoder() {
-    return m_dutyEncoderRight.getAbsolutePosition() - kOffsetRight;
+    return 1 - m_dutyEncoder.getAbsolutePosition() - kOffset;
   }
 
   /**
@@ -136,14 +116,11 @@ public class Intake extends SubsystemBase {
     return this.runOnce(
         () -> {
           if (position == "up") {
-            m_targetLeft = kUP;
-            m_targetRight = kUP;
+            m_target = kUP;
           } else if (position == "down") {
-            m_targetLeft = kDOWN;
-            m_targetRight = kDOWN;
+            m_target = kDOWN;
           } else if (position == "cone") {
-            m_targetLeft = kConeIntake;
-            m_targetRight = kConeIntake;
+            m_target = kConeIntake;
           }
         });
   }
@@ -158,37 +135,23 @@ public class Intake extends SubsystemBase {
     return this.run(
         () -> {
           if (position == "up") {
-            m_targetLeft = kUP;
-            m_targetRight = kUP;
+            m_target = kUP;
           } else if (position == "down") {
-            m_targetLeft = kDOWN;
-            m_targetRight = kDOWN;
+            m_target = kDOWN;
           } else if (position == "cone") {
-            m_targetLeft = kConeIntake;
-            m_targetRight = kConeIntake;
+            m_target = kConeIntake;
           }
         });
   }
 
   public double motorSpeedLeft() {
-    if (m_targetLeft == kConeIntake && !isOnTargetLeft()) return 0.06;
-    else return m_targetLeft - getLeftEncoder();
-  }
-
-  public double motorSpeedRight() {
-    if (m_targetRight == kConeIntake && !isOnTargetRight()) return 0.06;
-    else return m_targetRight - getRightEncoder();
+    if (m_target == kConeIntake && !isOnTargetLeft()) return 0.06;
+    else return m_target - getLeftEncoder();
   }
 
   private boolean isOnTargetLeft() {
 
     return (getLeftEncoder() < deadzone + kConeIntake && getLeftEncoder() > kConeIntake - deadzone);
-  }
-
-  private boolean isOnTargetRight() {
-
-    return (getRightEncoder() < deadzone + kConeIntake
-        && getRightEncoder() > kConeIntake - deadzone);
   }
 
   /**
@@ -211,5 +174,17 @@ public class Intake extends SubsystemBase {
             // setTarget("whatever");
           }
         });
+  }
+
+  public boolean limitSwitch() {
+    return !m_limitSwitch.get();
+  }
+
+  public Command spinnyBoi() {
+    return this.run(() -> m_wheelSpeed = 1);
+  }
+
+  public Command stop() {
+    return this.runOnce(() -> m_wheelSpeed = 0);
   }
 }
